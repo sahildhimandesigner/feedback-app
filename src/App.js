40481dc47e5b71fa4f9c68f15394d8228a61a66a
sahildhimandesigner <sahil@ucreate.co.in>
2022-01-08 20:01:40 +0530

    cleared the app component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 
 function App(){
-    const title =  'My Feedback application here';
-    const body = "This is my blog post";
-
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-    ]
-
-    const showcomment = true;
-
-    const commentBlock = (<ul>
-                {comments.map((comment, index) => (
-                    <li key={index}>{comment.text}</li>
-                ))}
-            </ul>)
-
     return(
         <>
-            {title} <br /> {body} <br />
-
-            {showcomment && commentBlock}
-            
+            <h1>My app</h1>            
         </>
     )
 }
