689c83219c843534ad4295de9b776a24b886f5db
sahildhimandesigner <sahil@ucreate.co.in>
2022-01-08 18:56:07 +0530

    without jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import React from "react";
+import react, { createElement } from "react";
+
 function App(){
-    return(<h1>My Feedback application here</h1>)
+    return react,createElement('div', {
+        className: 'container'},
+        React.createElement('h1', {}, 'My app')
+        )
 }
 
 export default App;
