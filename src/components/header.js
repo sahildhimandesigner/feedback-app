8afd2f84f66f6c9030579080262b97e5cba7a07a
sahildhimandesigner <sahil@ucreate.co.in>
2022-01-08 21:47:27 +0530

    how to pass the props to component and set the default value of component

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,15 @@
-function Header(){
+function Header({ text }){
     return(
         <header>
             <div className="container">
-                <h2>Feedback UI</h2>
+                <h2>{text}</h2>
             </div>
         </header>
     )
+}
+
+Header.defaultProps = {
+    text: 'Default PropType Feedback UI',
 }
 
 export default Header;
