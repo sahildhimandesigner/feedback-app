596b6bde69999995c4cc58c5ec2c4cb7fb9ca1b5
sahildhimandesigner <sahil@ucreate.co.in>
2022-01-08 19:55:23 +0530

    how to use condition in jsx and declare the var

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 
 function App(){
-    return(<h1>My Feedback application here</h1>)
+    const title =  'My Feedback application here';
+    const body = "This is my blog post";
+
+    const comments = [
+        {id: 1, text: 'Comment one'},
+        {id: 2, text: 'Comment two'},
+        {id: 3, text: 'Comment three'},
+    ]
+
+    const showcomment = true;
+
+    return(
+        <>
+            {title} <br /> {body} <br />
+
+            {showcomment ? (<ul>
+                {comments.map((comment, index) => (
+                    <li key={index}>{comment.text}</li>
+                ))}
+            </ul>) : 'no'}
+            
+        </>
+    )
 }
 
 export default App;
