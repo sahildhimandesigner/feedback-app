26992568c70dae5cb9a578aabacdc53efa991f95
sahildhimandesigner <sahil@ucreate.co.in>
2022-03-31 13:42:54 +0530

    define the state with default value

--- src/components/FeedbackItems.js
+++ src/components/FeedbackItems.js
@@ -1,8 +1,14 @@
+import { useState } from "react"
+
 function FeedbackItem(){
+    //Here we have define the state with default value
+    const [count, setCount] = useState(7);
+    const [text, setText] = useState('This is an example of a feeback item');
+
     return(
         <div className="card">
-            <div className="num-display">10</div>
-            <div className="text-display">This is an example of a feeback item</div>
+            <div className="num-display">{count}</div>
+            <div className="text-display">{text}</div>
         </div>
     )
 }
