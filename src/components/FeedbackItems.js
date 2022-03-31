2779a45dab16378f8e820962d5a42732db2b889c
sahildhimandesigner <sahil@ucreate.co.in>
2022-03-31 13:55:49 +0530

    created the function for increase the count and set default value

--- src/components/FeedbackItems.js
+++ src/components/FeedbackItems.js
@@ -2,13 +2,21 @@ import { useState } from "react"
 
 function FeedbackItem(){
     //Here we have define the state with default value
-    const [count, setCount] = useState(7);
+    const [rating, setRating] = useState(7);
     const [text, setText] = useState('This is an example of a feeback item');
+
+    //Created the function
+    const handleClick = () => {
+        setRating((prev)=> {            
+            return prev + 1;
+        })
+    }
 
     return(
         <div className="card">
-            <div className="num-display">{count}</div>
+            <div className="num-display">{rating}</div>
             <div className="text-display">{text}</div>
+            <button onClick={handleClick}>Click</button>
         </div>
     )
 }
