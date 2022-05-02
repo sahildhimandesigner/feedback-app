6183f8600e8632dafde91bdb9f2660d5ea68d846
sahildhimandesigner <sahil@ucreate.co.in>
2022-05-02 14:06:34 +0530

    added the close icon on post

--- src/components/FeedbackItems.js
+++ src/components/FeedbackItems.js
@@ -6,6 +6,9 @@ function FeedbackItem({item}){
     return(
         <Card>
             <div className="num-display">{item.rating}</div>
+            <button className='close'>
+                <FaTimes color='purple' />
+            </button>
             <div className="text-display">{item.text}</div>
         </Card>
     )
